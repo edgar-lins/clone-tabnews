2b82ad7e0f73f6152f05a564df7d09564af84f98
Edgar Lins <edgarlins.dev@gmail.com>
2024-12-12 16:45:14 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Não vou deixar que a morte alcance a mim, até eu ser o maior espadachim! 🚀</h1>
+  return (
+    <h1>
+      Não vou deixar que a morte alcance a mim, até eu ser o maior espadachim!
+      🚀
+    </h1>
+  );
 }
 
 export default Home;
